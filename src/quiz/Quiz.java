607d593b5e4797f32f3ec2408a9a1b8b64f80614
Christina Cai <christinacai@wustl.edu>
2022-03-25 16:02:02 -0500

    method progress

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -9,6 +9,10 @@ public class Quiz {
 	private String quiz_name;
 	private int num_questions;
 	
+	public Quiz() {
+		
+	}
+	
 	public Quiz(Question[] questions, String quiz_name, int num_questions) {
 		this.questions = questions;
 		this.quiz_name = quiz_name;
@@ -25,6 +29,41 @@ public class Quiz {
 	
 	public int getNumQuestions() {
 		return this.num_questions;
+	}
+	
+	public static void main(String[] args) {
+		Quiz new_quiz = new Quiz();
+	}
+	
+	public void createQuiz() {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("What is the name of the quiz?");
+		this.quiz_name = reader.next();
+		System.out.println("How many questions are in your quiz?");
+		this.num_questions = Integer.parseInt(reader.next());
+		
+		this.questions = new Question[this.num_questions];
+		for (int i = 0; i < this.num_questions; i++) {
+			this.questions[i] = addQuestion(i, reader);
+		}
+		
+	}
+	
+	public Question addQuestion(int num, Scanner reader) {
+		System.out.println("Question " + num);
+		String question = reader.next();
+		System.out.println("How many possible answers are there?");
+		int num_possible = Integer.parseInt(reader.next());
+		String[] possible_answers = new String[num_possible];
+		for (int i = 0; i < num_possible; i++) {
+			System.out.println("Answer Choice " + i);
+			String answer_choice = reader.next();
+			possible_answers[i] = answer_choice;
+		}
+		System.out.println("What is the correct answer?");
+		char correct = reader.next().charAt(0);
+		Question current_question = new Question(question, possible_answers, correct);
+		return current_question;
 	}
 	
 	public void takeQuiz() {
@@ -53,5 +92,6 @@ public class Quiz {
 		System.out.println("You got "+ percentage + "% of this quiz correct.");
 		return;
 	}
+	
 	
 }
