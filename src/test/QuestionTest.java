b6da32a11833b5f21e0532aca2038be3fb2076ec
jwchoi1128 <jiwonchoi@wustl.edu>
2022-03-25 15:57:51 -0500

    questionPrintTest

--- src/test/QuestionTest.java
+++ src/test/QuestionTest.java
@@ -34,6 +34,7 @@ class QuestionTest {
 		assertEquals(possible_answers, get_possible_answers);
 	}
 	
+	@Test
 	void get_correct_answer_test() {
 		String question = "Favorite color?";
 		String[] possible_answers = {"blue", "green", "red"};
@@ -46,5 +47,16 @@ class QuestionTest {
 		assertEquals(possible_answers, get_possible_answers);
 	}
 	
+	@Test
+	void testPrintQuestion() {
+		String question = "Favorite color?";
+		String[] possible_answers = {"blue", "green", "red"};
+		char correct_answer = 'B';
+		
+		Question question1 = new Question(question, possible_answers, correct_answer);
+		String display = question1.toString();
+		String expected = "Favorite color?\nA blue\nB green\nC red";
+		assertTrue(expected.equals(display));
+	}
 
 }
