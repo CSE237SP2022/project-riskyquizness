f256f473df02f29f9feec5f00e309c3144a0440c
jwchoi1128 <jiwonchoi@wustl.edu>
2022-03-29 16:54:38 -0500

    invalidInputTest

--- src/test/QuizTest.java
+++ src/test/QuizTest.java
@@ -159,4 +159,87 @@ class QuizTest {
 		assertTrue(expectedOutput.equals(display));
 	}
 	
+	@Test
+	void testUserInvalidInput() {
+		String prompt = "Favorite color?";
+		String[] possible_answers = {"blue", "green", "red"};
+		char correct_answer = 'B';
+		Question question1 = new Question(prompt, possible_answers, correct_answer);
+		
+		String prompt2 = "Favorite food?";
+		String[] possible_answers2 = {"orange", "rice", "bread"};
+		char correct_answer2 = 'A';
+		Question question2 = new Question(prompt2, possible_answers2, correct_answer2);
+		
+		ArrayList<Question> questions = new ArrayList<Question>();
+		questions.add(question1);
+		questions.add(question2);
+		String quiz_name = "TestQuiz";
+		int num_questions = 2;
+		Quiz quiz = new Quiz(questions, quiz_name, num_questions);
+		
+		String userInput = "b"+"\nB"+"\nA";
+		quiz.takeQuiz(new Scanner(userInput));
+		
+		String expectedOutput = "Favorite color?\nA blue\nB green\nC red\n"+"\nPlease enter a valid input.\nFavorite food?\nA orange\nB rice\nC bread\n\nYou got 100.0% of this quiz correct.";
+		String display = outputStreamCaptor.toString().trim();
+		
+		assertTrue(expectedOutput.equals(display));
+	}
+	
+	@Test
+	void testUserInvalidLengthInput() {
+		String prompt = "Favorite color?";
+		String[] possible_answers = {"blue", "green", "red"};
+		char correct_answer = 'B';
+		Question question1 = new Question(prompt, possible_answers, correct_answer);
+		
+		String prompt2 = "Favorite food?";
+		String[] possible_answers2 = {"orange", "rice", "bread"};
+		char correct_answer2 = 'A';
+		Question question2 = new Question(prompt2, possible_answers2, correct_answer2);
+		
+		ArrayList<Question> questions = new ArrayList<Question>();
+		questions.add(question1);
+		questions.add(question2);
+		String quiz_name = "TestQuiz";
+		int num_questions = 2;
+		Quiz quiz = new Quiz(questions, quiz_name, num_questions);
+		
+		String userInput = "Blue"+"\nB"+"\nA";
+		quiz.takeQuiz(new Scanner(userInput));
+		
+		String expectedOutput = "Favorite color?\nA blue\nB green\nC red\n"+"\nPlease enter a valid input.\nFavorite food?\nA orange\nB rice\nC bread\n\nYou got 100.0% of this quiz correct.";
+		String display = outputStreamCaptor.toString().trim();
+		
+		assertTrue(expectedOutput.equals(display));
+	}
+	
+	@Test
+	void testUserMultipleInvalidInput() {
+		String prompt = "Favorite color?";
+		String[] possible_answers = {"blue", "green", "red"};
+		char correct_answer = 'B';
+		Question question1 = new Question(prompt, possible_answers, correct_answer);
+		
+		String prompt2 = "Favorite food?";
+		String[] possible_answers2 = {"orange", "rice", "bread"};
+		char correct_answer2 = 'A';
+		Question question2 = new Question(prompt2, possible_answers2, correct_answer2);
+		
+		ArrayList<Question> questions = new ArrayList<Question>();
+		questions.add(question1);
+		questions.add(question2);
+		String quiz_name = "TestQuiz";
+		int num_questions = 2;
+		Quiz quiz = new Quiz(questions, quiz_name, num_questions);
+		
+		String userInput = "Blue"+"\ngreen"+"\nB"+"\nA";
+		quiz.takeQuiz(new Scanner(userInput));
+		
+		String expectedOutput = "Favorite color?\nA blue\nB green\nC red\n"+"\nPlease enter a valid input.\nPlease enter a valid input.\nFavorite food?\nA orange\nB rice\nC bread\n\nYou got 100.0% of this quiz correct.";
+		String display = outputStreamCaptor.toString().trim();
+		
+		assertTrue(expectedOutput.equals(display));
+	}
 }
